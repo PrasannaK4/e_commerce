c6c0e4f27c12049102039c6197079a31b6d6ebce
PrasannaK4 <166937006+PrasannaK4@users.noreply.github.com>
2024-06-17 13:19:20 +0530

    e_commerce starting pages changes

---
+++ src/Pages/Shop.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from '../Components/Hero/Hero'
+import Populars from '../Components/Popular/Populars'
+import Offers from '../Components/Offers/Offers'
+
+
+const Shop = () => {
+  return (
+    <div>
+        <Hero/>
+        <Populars/>
+        <Offers/>
+        
+    </div>
+  )
+}
+
+export default Shop
